3035c491cf9d11cc1832b0a07af9e4baa21a50d6
DAnawalt <101373506+DAnawalt@users.noreply.github.com>
2022-05-08 11:20:22 -0700

    myfirstcommit

--- Defender2022/Defender2022.py
+++ Defender2022/Defender2022.py
@@ -6,8 +6,9 @@ game = Game()
 game.add_sprite_to_game("SpaceShip", Player)
 
 # Main loop
-while game.running:
-    for event in pygame.event.get():
-        game.event_handler(event)
-    pressed_keys = pygame.key.get_pressed()
-    game.update_game(pressed_keys)
+if __name__ == "__main__":
+    while game.running:
+        for event in pygame.event.get():
+            game.event_handler(event)
+        pressed_keys = pygame.key.get_pressed()
+        game.update_game(pressed_keys)
